e92b002b901a5a7f7eb8f682bd3e03dc7bca182f
shrestharukesh1213 <125758326+shrestharukesh1213@users.noreply.github.com>
2023-09-20 20:49:19 +0545

    Completed logic code and project

--- script.js
+++ script.js
@@ -1,69 +1,141 @@
 let count = 1;
 
 let gameBoard = (() => {
-  const gameBoardArray = [, , , , , , , ,];
+  const gameBoardArray = [, , , , , , , , ,];
   return gameBoardArray;
 })();
-
-const player = (name) => {
-  const getName = () => name;
-  return getName;
-};
 
 const winner = (() => {
   const checkWinner = () => {
     if (
-      (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) ||
-      (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) ||
-      (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]) ||
-      (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) ||
-      (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) ||
-      (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8]) ||
-      (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) ||
-      (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6])
+      (gameBoard[0] === gameBoard[1] &&
+        gameBoard[1] === gameBoard[2] &&
+        gameBoard[0] === "X" &&
+        gameBoard[1] === "X" &&
+        gameBoard[2] === "X") ||
+      (gameBoard[3] === gameBoard[4] &&
+        gameBoard[4] === gameBoard[5] &&
+        gameBoard[3] === "X" &&
+        gameBoard[4] === "X" &&
+        gameBoard[5] === "X") ||
+      (gameBoard[6] === gameBoard[7] &&
+        gameBoard[7] === gameBoard[8] &&
+        gameBoard[6] === "X" &&
+        gameBoard[7] === "X" &&
+        gameBoard[8] === "X") ||
+      (gameBoard[0] === gameBoard[3] &&
+        gameBoard[3] === gameBoard[6] &&
+        gameBoard[0] === "X" &&
+        gameBoard[3] === "X" &&
+        gameBoard[6] === "X") ||
+      (gameBoard[1] === gameBoard[4] &&
+        gameBoard[4] === gameBoard[7] &&
+        gameBoard[1] === "X" &&
+        gameBoard[4] === "X" &&
+        gameBoard[7] === "X") ||
+      (gameBoard[2] === gameBoard[5] &&
+        gameBoard[5] === gameBoard[8] &&
+        gameBoard[2] === "X" &&
+        gameBoard[5] === "X" &&
+        gameBoard[8] === "X") ||
+      (gameBoard[0] === gameBoard[4] &&
+        gameBoard[4] === gameBoard[8] &&
+        gameBoard[0] === "X" &&
+        gameBoard[4] === "X" &&
+        gameBoard[8] === "X") ||
+      (gameBoard[2] === gameBoard[4] &&
+        gameBoard[4] === gameBoard[6] &&
+        gameBoard[2] === "X" &&
+        gameBoard[4] === "X" &&
+        gameBoard[6] === "X")
     ) {
-      return "Player1 is winner";
+      return "Player 1 is the winner";
     } else if (
-      (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) ||
-      (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) ||
-      (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]) ||
-      (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) ||
-      (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) ||
-      (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8]) ||
-      (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) ||
-      (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6])
+      (gameBoard[0] === gameBoard[1] &&
+        gameBoard[1] === gameBoard[2] &&
+        gameBoard[0] === "O" &&
+        gameBoard[1] === "O" &&
+        gameBoard[2] === "O") ||
+      (gameBoard[3] === gameBoard[4] &&
+        gameBoard[4] === gameBoard[5] &&
+        gameBoard[3] === "O" &&
+        gameBoard[4] === "O" &&
+        gameBoard[5] === "O") ||
+      (gameBoard[6] === gameBoard[7] &&
+        gameBoard[7] === gameBoard[8] &&
+        gameBoard[6] === "O" &&
+        gameBoard[7] === "O" &&
+        gameBoard[8] === "O") ||
+      (gameBoard[0] === gameBoard[3] &&
+        gameBoard[3] === gameBoard[6] &&
+        gameBoard[0] === "O" &&
+        gameBoard[3] === "O" &&
+        gameBoard[6] === "O") ||
+      (gameBoard[1] === gameBoard[4] &&
+        gameBoard[4] === gameBoard[7] &&
+        gameBoard[1] === "O" &&
+        gameBoard[4] === "O" &&
+        gameBoard[7] === "O") ||
+      (gameBoard[2] === gameBoard[5] &&
+        gameBoard[5] === gameBoard[8] &&
+        gameBoard[2] === "O" &&
+        gameBoard[5] === "O" &&
+        gameBoard[8] === "O") ||
+      (gameBoard[0] === gameBoard[4] &&
+        gameBoard[4] === gameBoard[8] &&
+        gameBoard[0] === "O" &&
+        gameBoard[4] === "O" &&
+        gameBoard[8] === "O") ||
+      (gameBoard[2] === gameBoard[4] &&
+        gameBoard[4] === gameBoard[6] &&
+        gameBoard[2] === "O" &&
+        gameBoard[4] === "O" &&
+        gameBoard[6] === "O")
     ) {
-      return "Player2 is winner";
+      return "Player 2 is the winner";
     }
   };
-  return { win: checkWinner() };
+  return { checkWinner };
 })();
 
-const newPlayer1 = player("Player1");
-const newPlayer2 = player("Player2");
 const displayBoard = document.querySelector(".gameboard");
 const displayBoardBoxes = document.querySelectorAll("[data-moveBox]");
+const playerTurnDisplay = document.querySelector(".playerTurn");
+const resetButton = document.querySelector(".reset");
 
 displayBoardBoxes.forEach((item) => {
   item.addEventListener("click", () => {
     moveBoxIndex = item.getAttribute("data-movebox");
-    if (gameBoard.length > 8) {
-      console.log(winner.win);
-    }
     if (item.textContent !== "") {
-      console.log("choose another spot");
+      item.addEventListener("click", (e) => {
+        e.preventDefault();
+      });
     } else if (item.textContent === "") {
       if (count % 2 == 0) {
-        console.log(item.getAttribute("data-movebox"));
-        item.textContent = "X";
-        gameBoard.splice(moveBoxIndex, 1, "X");
-      } else {
-        console.log(item.getAttribute("data-movebox"));
         item.textContent = "O";
         gameBoard.splice(moveBoxIndex, 1, "O");
+        playerTurnDisplay.textContent = "Player X's Turn";
+      } else {
+        item.textContent = "X";
+        gameBoard.splice(moveBoxIndex, 1, "X");
+        playerTurnDisplay.textContent = "Player O's Turn";
       }
     }
-    console.log(gameBoard);
     count++;
   });
+});
+
+displayBoard.addEventListener("click", () => {
+  if (winner.checkWinner()) {
+    playerTurnDisplay.textContent = winner.checkWinner();
+  }
+});
+
+resetButton.addEventListener("click", () => {
+  gameBoard = [, , , , , , , , ,];
+  displayBoardBoxes.forEach((item) => {
+    item.textContent = "";
+  });
+  count = 1;
+  playerTurnDisplay.textContent = "Player X's Turn";
 });
